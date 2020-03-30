d2f9089a10b35b2280cc0fb62cc255853fb2c759
julio.lopes@outsystems.com <julio.lopes@outsystems.com>
2020-03-30 14:58:03 +0100

    Update Doc

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,7 @@
 export default {
     typescript: true,
     dest: "/docs",
+    hashRouter: true,
     menu: ["Components"],
     themeConfig: {
         fontSizes: [10, 12, 14, 16, 20, 24, 32, 48]
